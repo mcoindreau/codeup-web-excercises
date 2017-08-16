58bdb3aab1574c71285ab8ce3a522f6d2dd32127
M Coindreau <mariannacoindreau@gmail.com>
2017-08-16 09:06:35 -0500

    Add conditional examples in if-else.js, switch_js and corresponding HTML

---
+++ js/external.js
@@ -0,0 +1,9 @@
+'use strict'
+
+console.log ('Hello from external JavaScript');
+
+alert('Welcome to my Website!');
+
+var colorAnswer = prompt('What is your favorite color?');
+
+alert('Great, ' + colorAnswer + ' is my favorite color too!');
