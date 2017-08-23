52915cdc572024e8fdf513e1242e41f134784ed0
M Coindreau <mariannacoindreau@gmail.com>
2017-08-23 17:57:42 -0500

    BOM - Defuse the BOM completed

--- js/objects.js
+++ js/objects.js
@@ -5,14 +5,25 @@
      * TODO:
      * Create person object, store it in a variable named person
      */
+    //var person = "Marianna";
+        // var object = {};
+    var person = new Object();
+    console.log("Person var is " + typeof person);
+
 
     /**
      * TODO:
      * Create firstName and lastName properties in your person object, and
      * assign your name to them
      */
+    person = {
+        firstName: "Marianna",
+        lastName: "Coindreau"
+    };
+    console.log(person);
 
-    /**
+
+/**
      * TODO:
      * Add a sayHello method to the person object that returns a greeting using
      * the firstName and lastName properties.
@@ -21,6 +32,13 @@
      * Example
      * person.sayHello() // returns "Hello from Rick Sanchez!"
      */
+    //person.sayHello = function(){
+    //    return "Hello from " + this.firstName + " " + this.lastName + "!";
+    //}; //literal function representation
+    //console.log("Hello from " + sayHello + "!");
+
+    var sayHello = (person.firstName + " " + person.lastName);
+       console.log("Hello from " + sayHello + "!");
 
     /** TODO: Remember this problem from before?
      *
@@ -36,11 +54,29 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
 
+    for (var i = 0; i < shoppers.length; i++){
+
+    var discount = 0;
+    var totalAfterDiscount = shoppers[i].amount;
+    // constant
+    //console.log("Cameron has to pay $180, he has discount of $0, the total after the discount is $0");
+
+    //1. variable
+
+    // 2. Conditionals
+    if (shoppers[i].amount > 200) {
+        discount = .1;
+        totalAfterDiscount = shoppers[i].amount - shoppers[i].amount * discount;
+    }
+
+    console.log(shoppers[i].name + " has to pay $" + shoppers[i].amount, "he has discount of $"+ discount + ", the total after the discount is $" + totalAfterDiscount);
+
+    }
 
 })();
