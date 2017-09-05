4a466319b2b7d1035a9f25b688f6eeca7918bd12
M Coindreau <mariannacoindreau@gmail.com>
2017-09-05 10:57:14 -0500

    completed traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,6 +1,7 @@
 "use strict";
 $(document).ready(function (){
 
+    //on click, displays FAQ answers. On click hides the answers.
     $('#highlight-important').click(function(event) {
         event.preventDefault();
         $('dd').toggleClass('invisible');
@@ -15,4 +16,48 @@ $(document).ready(function (){
         // $("dt").css('background-color', 'transparent' ); //get rid of all properties -- only allows one line to be highlighted at a time
         $(this).css('background-color', '#FFF7CB' );
     });
+
+    $("li").click(function(event) {
+        $("li").css('background-color', 'transparent' ); //get rid of all properties -- only allows one line to be highlighted at a time
+        $(this).css('background-color', '#FFF7CB' );
+        $("li").last().css('background-color', '#5E96FF' );
+    });
+
+    $("h3").click(function(event) {
+        $("h3").css('background-color', 'transparent' ); //get rid of   all properties -- only allows one line to be highlighted at a time
+        $(this).css('text-decoration', 'underline' );
+        $("li").last().css('font-weight', 'bold' );//not working
+    });
+
+    //on click, highlight li
+    $('#highlight-li').click(function(event) {
+        event.preventDefault();
+        $("li").last().css('background-color', '#FFF7CB' );
+        $("li").first().css('background-color', '#5E96FF' );
+    });
+
+    // $("#highlight-li").on("click", highlightAllLastListItems );
+    //
+    // function highlightAllLastListItems () {
+    //     $("ul").each(highlightLastListItem); //DOM element
+    // }
+    //
+    // function highlightLastListItem () {
+    //     console.log(this); //ul DOM
+    //     $("ul").each(highlightLastListItem); //'this' is each ul DOM element
+    //     $(this).children().last().css('background-color', '#FFF7CB' );
+    // }
+
+    $("h3").on("click", makeAllListItemsBold );
+
+    function makeAllListItemsBold () {
+            $(this).children().css('font-weight', 'bold' );
+        }
+
+
+    $("li").on("click", makeFirstSiblingBlue );
+
+    function makeFirstSiblingBlue () {
+        $(this).parent().children().first().css('color', '#5E96FF' );
+    }
 });
